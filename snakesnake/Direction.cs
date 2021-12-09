86c4baef4fa4175e2c924fc02af11ba2cb72b6bf
aniteddy <aniteddy1999@gmail.com>
2021-12-09 21:33:41 +0200

    Добавьте файлы проекта.

---
+++ snakesnake/Direction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace snakesnake
+{
+    /// <summary>
+    /// направления движения змейки
+    /// </summary>
+    enum Direction
+    {
+        LEFT,
+        RIGHT,
+        UP,
+        DOWN
+    }
+}
---
+++ snakesnake/HorizontalLine.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace snakesnake
+{
+    /// <summary>
+    /// горизонтальные линии
+    /// </summary>
+    class HorizontalLine : Figure
+    {
+        
+        public HorizontalLine(int xLeft, int xRight, int y, char sym)
+        {
+            pList = new List<Point>();
+            for (int x=xLeft; x<=xRight;x++)
+            {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+            }
+            
+        }
+
+
+    }
+}
